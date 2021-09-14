81a3e1cc2aa9bdad1fcde8d523d252d1b4c449e9
JoseVale99 <lc19160007@oaxaca.tecnm.mx>
2021-09-14 18:10:20 -0500

    create category

--- app/category.py
+++ app/category.py
@@ -1,8 +1,11 @@
 from app import app
 from flask import jsonify
-
+from flask_sqlalchemy import SQLALchemy
+from flask_marshmellow import Marshmallow
 
 #  rootes API REST
+
+# message of welcome
 @app.route('/', methods=['GET'])
 def index():
     return jsonify({'Message':"Welcome!"})
