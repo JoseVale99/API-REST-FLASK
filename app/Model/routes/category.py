919ef22a87b9bb0b2f959363e0336b2c993d3dac
JoseVale99 <lc19160007@oaxaca.tecnm.mx>
2021-09-15 00:42:26 -0500

    new path for routes/category

---
+++ app/Model/routes/category.py
@@ -0,0 +1,15 @@
+from app import app
+from flask import jsonify
+
+#  rootes API REST
+
+# message of welcome
+@app.route('/', methods=['GET'])
+def index():
+    return jsonify({'Message':"Welcome!"})
+
+
+
+
+
+    
