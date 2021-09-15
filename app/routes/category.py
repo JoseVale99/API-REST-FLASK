90083849c2288bd222d2760d8fe61a2ab780d69c
JoseVale99 <lc19160007@oaxaca.tecnm.mx>
2021-09-15 04:35:56 -0500

    method POST

--- app/routes/category.py
+++ app/routes/category.py
@@ -1,12 +1,12 @@
 from app import app
-from flask import jsonify
+from flask import jsonify, request
 from app.Model.models import Category,CategorySchema
-
+from flask_sqlalchemy import SQLAlchemy
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:123456789@localhost:3306/dbpythonAPI' 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-
+db = SQLAlchemy(app)
 #  rootes API REST
 
 # message of welcome
@@ -22,13 +22,29 @@ categories_schema = CategorySchema(many=True)
 
 # GET
 @app.route('/category',methods=['GET'])
-def get_categorias():
+def getCategories():
     all_categorias = Category.query.all()
     result = categories_schema.dump(all_categorias)
     return jsonify(result)
 
-
-      
-
+#  GET for ID
+@app.route('/category/<id>', methods=['GET'])      
+def getCategoryID(id):
+    category = Category.query.get(id)
+    return category_schema.jsonify(category)
+    
+#  POST
+@app.route('/categories', methods=['POST'])
+def categoryNew():
+    data = request.get_json(force=True)
+    cat_name = data['cat_name']
+    cat_description = data['cat_description']
+    
+    new_category = Category(cat_name,cat_description)
+    db.session.add(new_category)
+    db.session.commit()
+    return category_schema.jsonify(new_category)
+    
+    
 
     
