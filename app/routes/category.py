1e1e5d842152779d4c8f1c01eb673d7366e33bf6
JoseVale99 <lc19160007@oaxaca.tecnm.mx>
2021-09-15 03:51:48 -0500

    install dependencies

--- app/routes/category.py
+++ app/routes/category.py
@@ -1,5 +1,11 @@
 from app import app
 from flask import jsonify
+from app.Model.models import Category,CategorySchema
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:123456789@localhost:3306/dbpythonAPI' 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 
 #  rootes API REST
 
@@ -9,7 +15,20 @@ def index():
     return jsonify({'Message':"Welcome back!"})
 
 
+# One request
+category_schema = CategorySchema()
+#  Many requests
+categories_schema = CategorySchema(many=True)
 
+# GET
+@app.route('/category',methods=['GET'])
+def get_categorias():
+    all_categorias = Category.query.all()
+    result = categories_schema.dump(all_categorias)
+    return jsonify(result)
+
+
+      
 
 
     
