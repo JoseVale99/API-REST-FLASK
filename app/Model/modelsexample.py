61e7b001cb737481130b2829260eda53c9ad0896
JoseVale99 <lc19160007@oaxaca.tecnm.mx>
2021-09-15 03:59:00 -0500

    edit file example!

--- app/Model/modelsexample.py
+++ app/Model/modelsexample.py
@@ -1,26 +1,37 @@
-from flask import Flask
+from app import app # comment this line for running!
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
+# Create models for migration in Data Base... descomment for running
 
-# Create models for migration in Data Base.
-app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://USERNAME:PASSWORD@localhost:3306/DB_NAME' 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+# from flask import Flask
+# app = Flask(__name__)
 
 db = SQLAlchemy(app)
-ma  = Marshmallow(app)
 
-#  Model of category objects
+# Create table category 
 class Category(db.Model):
     cat_id = db.Column(db.Integer, primary_key=True)
-    cat_name = db.Column(db.String(100))
-    cat_description = db.Column(db.String(100))
+    cat_name = db.Column(db.String(110))
+    cat_description = db.Column(db.String(110))
 
     def __init__(self, cat_name, cat_description):
         self.cat_name = cat_name
         self.cat_description = cat_description
 
-db.create_all()
+ma  = Marshmallow(app)
+
+
+# Define Schema with Marshmallow
+# Schema category
+class CategorySchema(ma.Schema):
+    class Meta:
+        fields = ('cat_id','cat_name','cat_description')
+
+if __name__ == "__main__":
+    # Run this file directly to create the database tables.
+    print ("Creating database tables...")
+    db.create_all()
+    print( "Done!")
+
         
