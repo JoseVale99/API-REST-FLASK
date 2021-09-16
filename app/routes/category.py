9694f8c7585beffc0a3ecada66e376664eda9c9d
JoseVale99 <lc19160007@oaxaca.tecnm.mx>
2021-09-16 01:29:12 -0500

    API complete!

--- app/routes/category.py
+++ app/routes/category.py
@@ -1,49 +1,51 @@
 from app import app
 from flask import jsonify, request
-from app.Model.models import Category,CategorySchema
-from flask_sqlalchemy import SQLAlchemy
+from app.Model.models import  (Update,All,getDelete,
+getcatgoryID,getcatgoryPost,category_schema)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:123456789@localhost:3306/dbpythonAPI' 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
 #  rootes API REST
 
 # message of welcome
 @app.route('/', methods=['GET'])
 def index():
     return jsonify({'Message':"Welcome back!"})
-
-
-# One request
-category_schema = CategorySchema()
-#  Many requests
-categories_schema = CategorySchema(many=True)
-
-# GET
+             
+# GET all categories
 @app.route('/category',methods=['GET'])
 def getCategories():
-    all_categorias = Category.query.all()
-    result = categories_schema.dump(all_categorias)
+    result = All()
     return jsonify(result)
 
 #  GET for ID
 @app.route('/category/<id>', methods=['GET'])      
 def getCategoryID(id):
-    category = Category.query.get(id)
+    category = getcatgoryID(id)
     return category_schema.jsonify(category)
     
 #  POST
-@app.route('/categories', methods=['POST'])
+@app.route('/category', methods=['POST'])
 def categoryNew():
     data = request.get_json(force=True)
     cat_name = data['cat_name']
     cat_description = data['cat_description']
+    new_category = getcatgoryPost(cat_name,cat_description)
+    return jsonify('add successfully new category!')
     
-    new_category = Category(cat_name,cat_description)
-    db.session.add(new_category)
-    db.session.commit()
-    return category_schema.jsonify(new_category)
+# Update category
+@app.route('/category/<id>',methods=['PUT'])
+def UpdateCategory(id):
+    data = request.get_json(force=True)
+    cat_name = data['cat_name']
+    cat_description = data['cat_description']
+    categoryUpdate = Update(id,cat_name,cat_description)
+    return jsonify("update successfully!")
+    
+# DELETE
+@app.route('/category/<id>', methods=['DELETE'])
+def deleteCategory(id):
+    delete = getDelete(id)
+    return jsonify('delete successfully!')
     
     
 
